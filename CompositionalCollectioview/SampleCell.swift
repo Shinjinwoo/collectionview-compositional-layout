369cdba1bea54a1daf1bb314146f765e15a0050d
Shinjinwoo <jinu712@gmail.com>
2023-11-16 15:35:11 +0900

    feat: CollectionView 초기 데이터 세팅

---
+++ CompositionalCollectioview/SampleCell.swift
@@ -0,0 +1,12 @@
+//
+//  SampleCell.swift
+//  CompositionalCollectioview
+//
+//  Created by 신진우 on 11/16/23.
+//
+
+import UIKit
+
+class SampleCell: UICollectionViewCell {
+    
+}
--- CompositionalCollectioview/ViewController.swift
+++ CompositionalCollectioview/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    typealias Item = AppleFramework
+    enum Section {
+        case main
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configurationCollectionView()
+    
     }
-
-
+    
+    private func configurationCollectionView() {
+        
+        
+        
+    }
 }
 
